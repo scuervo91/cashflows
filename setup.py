a18f1e1f1943d1c085f8ebfe430cc47d7a63da4d
Santiago Cuervo <scuervo91@gmail.com>
2021-01-17 18:01:23 -0500

    change name

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-from setuptools import setup
+import setuptools
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='cashflows',
-      version='0.0.4',
-      description='Investment modeling and advanced engineering economics using Python',
-      long_description='Investment modeling and advanced engineering economics using Python',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Office/Business :: Financial',
-      ],
-      keywords='cashflow investments bonds depreciation loan irr',
-      url='http://github.com/jdvelasq/cashflows',
-      author='Juan D. Velasquez & Ibeth K. Vergara',
-      author_email='jdvelasq@unal.edu.co',
-      license='MIT',
-      packages=['cashflows'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      include_package_data=True,
-      zip_safe=False)
+setuptools.setup(
+    name="cashflows2", # Replace with your own username
+    version="v0.1",
+    author="Juan D. Velasquez & Ibeth K. Vergara. Forked Santiago Cuervo",
+    author_email="scuervo91@gmail.com",
+    description="'Investment modeling and advanced engineering economics using Python'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='cashflow investments bonds depreciation loan irr',
+    url="https://github.com/scuervo91/cashflows2",
+    download_url="https://github.com/scuervo91/cashflows2/archive/v0.1.tar.gz",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    package_data = {'':['*.csv','*.json']},
+    install_requires=[            
+        'numpy',
+        'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
